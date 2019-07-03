9b82e1a156511a3a9750d94a652a17af587d7f65
robel <robelabraham011@gmail.com>
2019-07-03 14:19:30 -0500

    helo helo

--- tipCalculator/calculateTip.js
+++ tipCalculator/calculateTip.js
@@ -1,10 +1,10 @@
 function calculateTotal() {
-        var subtotalElem = document.getElementById('subtotal');
-        var tipElem = document.getElementById('tip');
-        var totalElem = document.getElementById('total');
-        var subtotal = parseInt(subtotalElem.value);
-        var tip = parseInt(tipElem.value);
-        var total =( subtotal ) + ((tip*subtotal)/100);
-        totalElem.innerHTML = '$' + total;
+    var subtotalElem = document.getElementById('subtotal');
+    var tipElem = document.getElementById('tip');
+    var totalElem = document.getElementById('total');
+    var subtotal = parseFloat(subtotalElem.value);
+    var tip = parseFloat(tipElem.value);
+    var total = (subtotal) + ((tip * subtotal) / 100);
+    totalElem.innerHTML = '$' + total;
 
 }
